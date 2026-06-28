8a80134d2ff54e32c1a3cb61955f3feda856d862
M0JXD <jamiedrinkell@gmail.com>
2026-06-28 21:01:55 +0100

    Basic first version

--- src/platforms/love/main.lua
+++ src/platforms/love/main.lua
@@ -11,7 +11,7 @@ Expert: ~16px
 local swpr = require('logic')
 local cell_imgs, start_menu, arrow
 local mode, size, scale = 0, 64
-local columns, rows, board
+local columns, rows, board, situation
 
 local function draw_menu()
 	love.graphics.draw(start_menu)
@@ -19,27 +19,49 @@ local function draw_menu()
 end
 
 local function draw_game()
-	for i = 0, columns - 1 do
-		for k = 0, rows - 1 do
-			love.graphics.drawLayer(cell_imgs, 10, (i * size), 640 - (size * rows) + (k * size), 0,
-				scale, scale)
+	for i = 0, rows - 1 do
+		for k = 0, columns - 1 do
+			if board[i+1][k+1] == nil then
+				love.graphics.drawLayer(cell_imgs, 10, (i * size), 640 - (size * rows) + (k * size),
+					0, scale, scale)
+			elseif board[i+1][k+1] == 'M' then
+				if situation ~= 'win' then situation = 'lost' end
+				love.graphics.drawLayer(cell_imgs, 14, (i * size), 640 - (size * rows) + (k * size),
+					0, scale, scale)
+			elseif board[i+1][k+1] == 'F' then
+				love.graphics.drawLayer(cell_imgs, 11, (i * size), 640 - (size * rows) + (k * size),
+					0, scale, scale)
+			else
+				love.graphics.drawLayer(cell_imgs, board[i+1][k+1] + 1, (i * size),
+					640 - (size * rows) + (k * size), 0, scale, scale)
+			end
+
 		end
 	end
 end
 
 function menu_mousemoved(x, y, dx, dy, istouch)
-	if y < 420 then mode = 0
-	elseif y < 540 then mode = 105
-	else mode = 205 end
+	if y < 420 then
+		mode = 0
+	elseif y < 540 then
+		mode = 105
+	else
+		mode = 205
+	end
 end
 
 function menu_mousepressed(x, y, button, istouch, presses)
-	if y < 420 then mode = 'b'
-	elseif y < 540 then mode = 'i'
-	else mode = 'e' end
-	columns, rows, board = swpr.get_details(mode)
+	if y < 420 then
+		mode = 'b'
+	elseif y < 540 then
+		mode = 'i'
+	else
+		mode = 'e'
+	end
 	size = mode == 'b' and 64 or mode == 'i' and 32 or 16
 	scale = size / 320
+	situation = 'new'
+	columns, rows, board = swpr.get_details(mode)
 	love.draw = draw_game
 	love.mousemoved = game_mousemoved
 	love.mousepressed = game_mousepressed
@@ -50,7 +72,23 @@ function game_mousemoved(x, y, dx, dy, istouch)
 end
 
 function game_mousepressed(x, y, button, istouch, presses)
+	if y < 640 - (size * rows) then return end
 
+	-- Convert x/y to grid
+	local grid_x = math.floor(x / size) + 1
+	local grid_y = math.floor((y - (640 - (size * rows))) / size) + 1
+
+	if button == 1 then
+		if situation == 'new' then
+			board, situation = swpr.setup_game(grid_x, grid_y, mode)
+		else
+			board, situation = swpr.sweep_cell(grid_x, grid_y)
+		end
+	elseif button == 2 then
+		if situation ~= 'new' then
+			board, situation = swpr.toggle_flag(grid_x, grid_y)
+		end
+	end
 end
 
 love.mousemoved = menu_mousemoved
